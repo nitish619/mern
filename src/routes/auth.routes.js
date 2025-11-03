f455f155d1262c3d1ca86ab16e9ba94b790b8ccf
Nitish Chavan <nitishchavan937@gmail.com>
2025-11-03 09:51:08 +0530

    138

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,15 +1,22 @@
 import { Router } from "express";
-import { login, registerUser } from "../controllers/Auth.controller.js";
+import {
+  login,
+  logoutUser,
+  registerUser,
+} from "../controllers/Auth.controller.js";
 import { validate } from "../middlewares/validator.middleware.js";
 import {
   userLoginValidator,
   userRegisterValidator,
 } from "../validator/index.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/register").post(userRegisterValidator(), validate, registerUser);
 
 router.route("/login").post(userLoginValidator(), validate, login);
+
+router.route("/logout").post(verifyJWT, logoutUser);
 
 export default router;
